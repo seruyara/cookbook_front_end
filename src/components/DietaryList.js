347c5b89c810b1bf11cf14ec07efad6766586180
Sheila Seruya <Sheilaseruya@gmail.com>
2023-06-12 07:14:40 +0300

    adedd a dietarie list and form

--- src/components/DietaryList.js
+++ src/components/DietaryList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const DietaryList = () => {
   const [dietaries, setDietaries] = useState([]);
+  const [newDietary, setNewDietary] = useState('');
 
   useEffect(() => {
     fetchDietaries();
@@ -21,9 +22,39 @@ const DietaryList = () => {
     }
   };
 
+  const handleInputChange = (e) => {
+    setNewDietary(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:9292/dietaries', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ preference: newDietary }),
+      });
+      if (response.ok) {
+        // Refresh the dietaries list
+        fetchDietaries();
+        setNewDietary('');
+      } else {
+        console.error('Failed to create dietary');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h2>Dietaries</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={newDietary} onChange={handleInputChange} />
+        <button type="submit">Add Dietary</button>
+      </form>
       {dietaries.map((dietary) => (
         <div key={dietary.id}>
           <h3>{dietary.preference}</h3>
